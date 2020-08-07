0019332a65cfc0198d63015eac2268c8c3991452
Richard <rlii@uwo.ca>
2020-08-07 13:24:26 -0400

    updated editor form

--- src/ultimateInventory/NutsEditorController.java
+++ src/ultimateInventory/NutsEditorController.java
@@ -1,69 +1,255 @@
 package ultimateInventory;
 
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
 
-public class NutsEditorController {
+public class NutsEditorController implements Initializable {
 
     private NutsTableManager nutsTableManager;
 
-
+    //************************************************************_____ADD FORM INPUTS_____*****************************************************************
     @FXML
     Button saveBtn;
-
     @FXML
     TextField addPriceText;
     @FXML
     TextField addIdText;
+    @FXML
+    TextField addVendorNameText;
+    @FXML
+    TextField addVendorEmailText;
+    @FXML
+    TextField addVendorContactText;
+    @FXML
+    TextField addVendorWebsiteText;
+    @FXML
+    RadioButton addCadBtn;
+    @FXML
+    RadioButton addUsdBtn;
+    @FXML
+    DatePicker addPurchaseDateText;
+    @FXML
+    TextField addPayTermText;
+    @FXML
+    TextField addPayMethodText;
+    @FXML
+    TextArea addNoteText;
+
+    //************************************************************_____EDIT FORM INPUTS_____*****************************************************************
+    @FXML
+    Button updateBtn;
+    @FXML
+    TextField editPriceText;
+    @FXML
+    TextField editIdText;
+    @FXML
+    TextField editVendorNameText;
+    @FXML
+    TextField editVendorEmailText;
+    @FXML
+    TextField editVendorContactText;
+    @FXML
+    TextField editVendorWebsiteText;
+    @FXML
+    RadioButton editCadBtn;
+    @FXML
+    RadioButton editUsdBtn;
+    @FXML
+    DatePicker editPurchaseDateText;
+    @FXML
+    TextField editPayTermText;
+    @FXML
+    TextField editPayMethodText;
+    @FXML
+    TextArea editNoteText;
+    @FXML
+    ComboBox editCombox;
+    @FXML
+    Label editIdLabel;
+    @FXML
+    Label editVendorNameLabel;
+    @FXML
+    Label editVendorEmailLabel;
+    @FXML
+    Label editVendorContactLabel;
+    @FXML
+    Label editVendorWebsiteLabel;
+    @FXML
+    Label editPriceLabel;
+    @FXML
+    Label editPayMethodLabel;
+    @FXML
+    Label editPayTermLabel;
+    @FXML
+    Label editPayDateLabel;
+    @FXML
+    Label editCurrLabel;
+    @FXML
+    Label editNoteLabel;
+
+    //************************************************************_____DELETE FORM INPUTS_____*****************************************************************
+    @FXML
+    Button deleteBtn;
+    @FXML
+    ComboBox delCombox;
+    @FXML
+    Label delIdLabel;
+    @FXML
+    Label delVendorNameLabel;
+    @FXML
+    Label delVendorEmailLabel;
+    @FXML
+    Label delVendorContactLabel;
+    @FXML
+    Label delVendorWebsiteLabel;
+    @FXML
+    Label delPriceLabel;
+    @FXML
+    Label delPayMethodLabel;
+    @FXML
+    Label delPayTermLabel;
+    @FXML
+    Label delPayDateLabel;
+    @FXML
+    Label delCurrLabel;
+    @FXML
+    Label delNoteLabel;
+
+    //All use variables
+    Boolean isCad = true;
+    ObservableList<String> data = FXCollections.observableArrayList();
 
     public void setModel(NutsTableManager nut)
     {
         nutsTableManager = nut;
+        buildComBoxData();
     }
 
-    public void newNut(ActionEvent actionEvent) {
+    public void add(ActionEvent actionEvent) {
         try
         {
-            nutsTableManager.addNewNut();
+
+            String curr;
+            if(isCad == true)
+            {
+                curr = "CAD";
+            }
+            else
+            {
+                curr = "USD";
+            }
+
+            nutsTableManager.add(addIdText.getText(),
+                    addVendorNameText.getText(),
+                    addVendorEmailText.getText(),
+                    addVendorContactText.getText(),
+                    addVendorWebsiteText.getText(),
+                    Integer.valueOf(addPriceText.getText()),
+                    addPayMethodText.getText(),
+                    addPayTermText.getText(),
+                    addPurchaseDateText.getEditor().getText(),
+                    curr,
+                    addNoteText.getText());
         }
-        catch(Exception e)
+        catch(SQLException e)
         {
-            displayAlert("Error!");
+            displayAlert("Error!" + e);
+        }
+        finally {
+            Stage stage = (Stage) saveBtn.getScene().getWindow();
+            stage.close();
         }
     }
-    public void editNut()
+    public void edit()
     {
         try
         {
-            nutsTableManager.editNut();
+            String curr;
+            if(isCad == true)
+            {
+                curr = "CAD";
+            }
+            else
+            {
+                curr = "USD";
+            }
+            nutsTableManager.edit(editCombox.getValue().toString(),
+                    editVendorNameText.getText(),
+                    editVendorEmailText.getText(),
+                    editVendorContactText.getText(),
+                    editVendorWebsiteText.getText(),
+                    Integer.valueOf(editPriceText.getText()),
+                    editPayMethodText.getText(),
+                    editPayTermText.getText(),
+                    editPurchaseDateText.getEditor().getText(),
+                    curr,
+                    editNoteText.getText());
         }
         catch(Exception e)
         {
-            displayAlert("Error!");
+            displayAlert("Error!" + e);
+        }
+        finally {
+            Stage stage = (Stage) updateBtn.getScene().getWindow();
+            stage.close();
         }
     }
-    public void deleteNut()
+    public void delete()
     {
         try
         {
-            nutsTableManager.deleteNut();
+            nutsTableManager.delete(delCombox.getValue().toString());
         }
-        catch(Exception e)
+        catch(SQLException e)
         {
-            displayAlert("Error!");
+            displayAlert("Error! :" + e);
+        }
+        finally {
+            Stage stage = (Stage) deleteBtn.getScene().getWindow();
+            stage.close();
+        }
+    }
+    public void cadCall()
+    {
+        addCadBtn.setSelected(true);
+        editCadBtn.setSelected(true);
+        addUsdBtn.setSelected(false);
+        editUsdBtn.setSelected(false);
+        isCad = true;
+    }
+    public void usdCall()
+    {
+        addUsdBtn.setSelected(true);
+        editUsdBtn.setSelected(true);
+        addCadBtn.setSelected(false);
+        editCadBtn.setSelected(false);
+        isCad = false;
+    }
+    public void buildComBoxData()
+    {
+        try
+        {
+            data.addAll(nutsTableManager.getIdNames());
+        }
+        catch(SQLException e)
+        {
+            displayAlert("ERROR: "+e);
         }
     }
     private void displayAlert(String msg) {
@@ -85,5 +271,74 @@ public class NutsEditorController {
         } catch (IOException ex1) {
 
         }
+    }
+    public void updateDelLabels()
+    {
+        try
+        {
+            String id = delCombox.getValue().toString();
+            String [] array = nutsTableManager.getRow(id);
+            delVendorNameLabel.setText(array[0]);
+            delVendorEmailLabel.setText(array[1]);
+            delVendorContactLabel.setText(array[2]);
+            delVendorWebsiteLabel.setText(array[3]);
+            delPriceLabel.setText(array[4]);
+            delPayMethodLabel.setText(array[5]);
+            delPayTermLabel.setText(array[6]);
+            delPayDateLabel.setText(array[7]);
+            delCurrLabel.setText(array[8]);
+            delNoteLabel.setText(array[9]);
+        }
+        catch(SQLException e)
+        {
+            displayAlert("ERROR : "+e);
+        }
+    }
+    public void updateEditLabels()
+    {
+        try
+        {
+            String id = editCombox.getValue().toString();
+            String [] array = nutsTableManager.getRow(id);
+            editVendorNameLabel.setText(array[0]);
+            editVendorEmailLabel.setText(array[1]);
+            editVendorContactLabel.setText(array[2]);
+            editVendorWebsiteLabel.setText(array[3]);
+            editPriceLabel.setText(array[4]);
+            editPayMethodLabel.setText(array[5]);
+            editPayTermLabel.setText(array[6]);
+            editPayDateLabel.setText(array[7]);
+            editCurrLabel.setText(array[8]);
+            editNoteLabel.setText(array[9]);
+
+            editVendorNameText.setText(array[0].trim());
+            editVendorEmailText.setText(array[1].trim());
+            editVendorContactText.setText(array[2].trim());
+            editVendorWebsiteText.setText(array[3].trim());
+            editPriceText.setText(array[4].trim());
+            editPayMethodText.setText(array[5].trim());
+            editPayTermText.setText(array[6].trim());
+            editPurchaseDateText.getEditor().setText(array[7].trim());
+            if(array[8].trim().equals("CAD"))
+            {
+                cadCall();
+            }
+            else
+            {
+                usdCall();
+            }
+            editNoteText.setText(array[9].trim());
+
+        }
+        catch(SQLException e)
+        {
+            displayAlert("Error :" + e);
+        }
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        editCombox.setItems(data);
+        delCombox.setItems(data);
     }
 }
--- src/ultimateInventory/NutsTableManager.java
+++ src/ultimateInventory/NutsTableManager.java
@@ -1,8 +1,11 @@
 package ultimateInventory;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import org.apache.derby.client.am.SqlException;
+
+import javax.xml.transform.Result;
+import java.sql.*;
 
 public class NutsTableManager {
     Connection connection;
@@ -22,22 +25,86 @@ public class NutsTableManager {
             finally
             {
                 stmt.execute("CREATE TABLE Nuts ("
-                        + "NutID CHAR(15),"
+                        + "ID CHAR(30) NOT NULL PRIMARY KEY,"
+                        + "VendorName CHAR(30),"
+                        + "VendorEmail CHAR (30),"
+                        + "VendorContact CHAR (30),"
+                        + "VendorWebsite CHAR (30),"
                         + "Price INT,"
-                        + "Manufacturer CHAR(30)" + ")");
+                        + "PayMethod CHAR(30),"
+                        + "PayTerm CHAR(30),"
+                        + "PurchaseDate CHAR(10),"
+                        + "Currency CHAR(3),"
+                        + "Note VARCHAR (20000)"+ ")");
             }
         }
     }
-    public void addNewNut()
+    public void add(String id, String vName, String vEmail, String vContact, String vWebsite, int price, String pMethod, String pTerm, String date, String curr, String Note) throws SQLException
     {
+        String sql = "INSERT INTO Nuts(ID, VendorName, VendorEmail, VendorContact, VendorWebsite, Price, PayMethod, PayTerm, PurchaseDate, Currency, Note) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        PreparedStatement prep = connection.prepareStatement(sql);
+        prep.setString(1,id);
+        prep.setString(2,vName);
+        prep.setString(3,vEmail);
+        prep.setString(4,vContact);
+        prep.setString(5,vWebsite);
+        prep.setInt(6,price);
+        prep.setString(7,pMethod);
+        prep.setString(8,pTerm);
+        prep.setString(9,date);
+        prep.setString(10,curr);
+        prep.setString(11,Note);
+        prep.executeUpdate();
+    }
+    public void edit(String id, String vName, String vEmail, String vContact, String vWebsite, int price, String pMethod, String pTerm, String date, String curr, String Note)throws SQLException
+    {
+        Statement stmt = connection.createStatement();
+        stmt.executeUpdate("UPDATE Nuts SET Price ="+price+"," +
+                "VendorName ="+vName+"," +
+                "VendorEmail = "+vEmail+"," +
+                "VendorContact ="+vContact+"," +
+                "VendowWebsite ="+vWebsite+"," +
+                "Currency ="+curr+"," +
+                "PurchaseDate="+date+"," +
+                "PayTerm = "+pTerm+"," +
+                "PayMethod ="+pMethod+"," +
+                "Note ="+Note+"" +
+                " WHERE ID = '"+id+"'");
+
 
     }
-    public void editNut()
+    public void delete(String id) throws SQLException
     {
-
+        String sql = "DELETE FROM Nuts WHERE ID = ?";
+        PreparedStatement stmt = connection.prepareStatement(sql);
+        stmt.setString(1,id);
+        stmt.executeUpdate();
     }
-    public void deleteNut()
+    public ObservableList<String> getIdNames() throws SQLException
     {
+        ObservableList<String> list = FXCollections.observableArrayList();
+        ResultSet rs;
+        Statement stmt = connection.createStatement();
+        rs = stmt.executeQuery("SELECT ID FROM Nuts");
+        while(rs.next())
+        {
+            list.add(rs.getString(1));
+        }
 
+        return list;
+    }
+    public String [] getRow(String id) throws SQLException
+    {
+        String sql = "SELECT * FROM Nuts WHERE ID = ?";
+        PreparedStatement stmt = connection.prepareStatement(sql);
+        stmt.setString(1,id);
+        ResultSet rs = stmt.executeQuery();
+        rs.next();
+        String [] array = new String [10];
+        for(int x = 0; x< 10; x++)
+        {
+            array[x] = rs.getString(x+2);
+        }
+        return array;
     }
 }
